7e27be20c0b16d659e38b6f58e13a0a4ab237d5e
Gerard <gerard.g.johnson@gmail.com>
2023-04-28 23:44:29 -0500

    made changes with index,app, app2

--- js/app.js
+++ js/app.js
@@ -1,64 +1,74 @@
-'use strict'
+"use strict";
+
 let nombre = prompt("Welcome to my game! What is your name?");
+alert("Greetings" + username + "!")
 
-let answerOne = prompt("Did I take a web design class?");
+let answerOne = prompt("Did I take a web design class?").toLowerCase();
+
+if (answerOne === 'y' || answerOne === 'yes') {
+    alert("¡Así es!")
+    console.log("¡Así es!");
+} else {
+    alert("Está mal")
+    console.log("Está mal");
+}
+
 let answerTwo = prompt("Can I speak another language?");
+if (answerTwo === 'y' || answerTwo === 'yes') {
+    alert("¡Así es!")
+    console.log("¡Así es!");
+
+} else {
+    alert("Está mal")
+    console.log("Está mal");
+}
+
 let answerThree = prompt("Is my favorite color yellow");
-let answerFour = prompt("Have I traveled outside of the US");
-let answerFive = prompt("Am I left-handed?");
-
-
-if(answerOne === 'y' || answerOne === 'yes'){
-    console.log("¡Así es!");
-} else {
-    console.log("Está mal");
-}
-
-if(answerTwo === 'y' || answerTwo === 'yes'){
-    console.log("¡Así es!");
-} else {
-    
-    console.log("Está mal");
-}
-
 if(answerThree === 'n' || answerThree === 'no'){
+    alert("¡Así es!")
     console.log("¡Así es!");
+
 } else {
-    
+    alert("Está mal")
     console.log("Está mal");
 }
 
+let answerFour = prompt("Have I traveled outside of the US");
 if(answerFour === 'n' || answerFour === 'no'){
+    alert("¡Así es!")
     console.log("¡Así es!");
+
 } else {
-    
+    alert("Está mal")
     console.log("Está mal");
 }
 
+let answerFive = prompt("Am I left-handed?");
 if(answerFive === 'y' || answerFive === 'yes'){
+    alert("¡Así es!")
     console.log("¡Así es!");
+
 } else {
-    
+    alert("Está mal")
     console.log("Está mal");
 }
 
-let response;
-let correctAnswer = 3
-let x;
 
-response = prompt("Can you guess a number for me");
-console.log("You said: " + x)
+let correctAnswer;
+correctAnswer = "3";
+let attempts = 4;
 
-x = parseInt(response)
-
-if (response > correctAnswer) {
-    alert("Too High");
-} else if (response > correctAnswer)
-    alert("Too Low"); {  
-} else if (x === correctAnswer){
-        alert("Awesome, you got it!")
+while (attempts > 0) {
+let newanswer = prompt("Can you guess a number for me");
+    if (correctAnswer === newanswer) {
+        console.log("You're right. Congrats!");
+        alert("You're right. Congrats!");
+        break;
+    } else {
+        console.log("You're incorrect. Try again.");
+        alert("You're incorrect. Try again.");
+        attempts = attempts - 1;
     }
-    
 
 let x = 3
 while(x === "3") {
